54cad2d4ba672fdfd38f976faecbc4f65a0efadc
Alan Sobchacki <a.sobchack@gmail.com>
2024-12-31 00:50:39 -0300

    add functional JWT auth system.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../scopes/authenticationUser/authenticationUser.service';
 import { FindUserByEmailDto } from '../scopes/authenticationUser/dto/find-user-by-email.dto';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const findUserByEmailDto: FindUserByEmailDto = { email };
     const user = await this.usersService.findByEmail(findUserByEmailDto);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
   
     if (user && await bcrypt.compare(password, user.password)) {
       return { ...user, password: undefined };
     }
 
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { useremail: user.email, sub: user.id }
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
