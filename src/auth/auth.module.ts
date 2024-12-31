bff57c12a3cf4089230c547f40ade8139105d6b1
Alan Sobchacki <a.sobchack@gmail.com>
2024-12-31 01:21:08 -0300

    finish implementing a jwt auth system.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthenticationUserModule } from '../scopes/authenticationUser/authentic
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 import { jwtConstants } from './constants';
 
 @Module({
@@ -12,10 +13,10 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '15m' },
     })
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 
