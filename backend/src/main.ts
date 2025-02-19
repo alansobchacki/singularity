b9aa8c322ced84d1c5fb64be05ddaeee4d94b2a6
Alan Sobchacki <a.sobchack@gmail.com>
2025-02-19 10:45:15 -0300

    Tweak api routing parameters.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
