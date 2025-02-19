95168b041cd1b53df243ed4b1ccdee58a5e1f817
Alan Sobchacki <a.sobchack@gmail.com>
2025-02-19 11:52:37 -0300

    Configure CORS settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,8 +7,16 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendOrigin = process.env.FRONTEND_ORIGIN;
+
+  app.enableCors({
+    origin: frontendOrigin,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
