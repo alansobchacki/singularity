1467ee034764610a521cace095b5e566564e0c00
Alan Sobchacki <a.sobchack@gmail.com>
2025-04-07 12:10:00 -0300

    Add a GET hello world request at main page to help mitigate API cold starts

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,14 +3,18 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-// This will / should become a landing page in the future.
-// For now, it should only redirect users to the login page
 export default function LandingPage() {
   const router = useRouter();
   const [mounted, setMounted] = useState(false);
 
+  // Used to fire up the API and help mitigate cold starts
   useEffect(() => {
     setMounted(true);
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/api/v1" || "http://localhost:3800/api/v1")
+    .then(() => console.log("API warmed up"))
+    .catch((err) => console.error("API warm-up failed:", err));
+
     router.replace("/login");
   }, [router]);
 
